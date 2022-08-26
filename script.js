68514e160b73718a8b37821dc319e0509db94fd3
Pouyan <pouyannc@gmail.com>
2022-08-26 17:02:18 -0400

    Create square grid for sketchpad

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(1);
+
+const gridContainer = document.querySelector(".gridContainer");
+const gridContainerWidth = gridContainer.clientWidth;
+
+let boxesPerRow = 16;
+let boxesTotal = boxesPerRow ** 2;
+let boxWidth = parseFloat (gridContainerWidth) / boxesPerRow + "px";
+console.log(boxWidth);
+
+for (let i = 0; i < boxesTotal; i++) {
+    const box = document.createElement("div");
+    box.classList.add("grid");
+    box.style.width = boxWidth;
+    box.style.height = boxWidth;
+    gridContainer.appendChild(box);
+    console.log(boxWidth);
+}
