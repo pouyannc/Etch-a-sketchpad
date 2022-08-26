8a886d4ebe59272d11b0c551b4c383e1b61cee2f
Pouyan <pouyannc@gmail.com>
2022-08-26 18:20:01 -0400

    Add option to change grid size

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+function clearGrid () {
+    gridContainer.innerHTML = "";
+}
+
+function createGrid () {
+    clearGrid();
+    boxesTotal = boxesPerRow ** 2;
+    for (let i = 0; i < boxesTotal; i++) {
+        const box = document.createElement("div");
+        box.classList.add("grid");
+        box.style.width = boxWidth;
+        box.style.height = boxWidth;
+        gridContainer.appendChild(box);
+    }
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach(b => b.addEventListener("mouseover", function() {
+        b.style.background = drawColor; 
+        b.style.borderColor = drawColor;} ));
+}
 
 const gridContainer = document.querySelector(".gridContainer");
 const gridContainerWidth = gridContainer.clientWidth;
+const gridSizeButton = document.querySelector(".gridSize");
 
 let boxesPerRow = 16;
 let boxesTotal = boxesPerRow ** 2;
 let boxWidth = parseFloat (gridContainerWidth) / boxesPerRow + "px";
+let drawColor = "black";
 
-for (let i = 0; i < boxesTotal; i++) {
-    const box = document.createElement("div");
-    box.classList.add("grid");
-    box.style.width = boxWidth;
-    box.style.height = boxWidth;
-    gridContainer.appendChild(box);
-}
+gridSizeButton.addEventListener("click", function (){
+    boxesPerRow = prompt("Enter squares per side:");
+    boxWidth = parseFloat (gridContainerWidth) / boxesPerRow + "px";
+    createGrid();
+});
 
-const grid = document.querySelectorAll(".grid");
-grid.forEach(b => b.addEventListener("mouseover", function() {b.style.background = "black";} ));
+createGrid();
+
